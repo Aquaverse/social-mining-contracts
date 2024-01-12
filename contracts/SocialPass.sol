d2c21afb3011881c035d3f2ef829bb90f4eca7b0
coralsdm <fqchen@sending.tech>
2024-01-12 21:10:49 +0800

    add SocialPass.sol

--- contracts/SocialPass.sol
+++ contracts/SocialPass.sol
@@ -1,21 +1,64 @@
-// contracts/SocialPass.sol
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
-contract SocialPass {
-    uint256 private _value;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-    // Emitted when the stored value changes
-    event ValueChanged(uint256 value);
+contract SocialPass is ERC721, ERC721URIStorage, ERC721Pausable, Ownable {
+    uint256 private _nextTokenId;
 
-    // Stores a new value in the contract
-    function store(uint256 value) public {
-        _value = value;
-        emit ValueChanged(value);
+    constructor(address initialOwner)
+        ERC721("Social Pass", "SCP")
+        Ownable(initialOwner)
+    {}
+
+    function pause() public onlyOwner {
+        _pause();
     }
 
-    // Reads the last stored value
-    function retrieve() public view returns (uint256) {
-        return _value;
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function selfMint(string memory uri) public {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address to, uint256 tokenId, address auth)
+        internal
+        override(ERC721, ERC721Pausable)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
